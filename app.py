05ff00ebcb58bbbeda082516f506dc6e1315f0ab
Alana Queiroz <166722191+alanaqroz@users.noreply.github.com>
2025-11-11 14:31:49 +0000

    add correção

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def metricas():
 
     return jsonify(dados)
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     app.run(host= '0.0.0.0', port= int(os.environ.get("PORT", 500)))
 # Obtém versão do S.O. subjacente
 #print(platform.platform())
